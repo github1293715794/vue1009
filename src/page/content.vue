d1dedc3d53812f7be3c585750176886f98722589
github1293715794 <1293715794@qq.com>
2018-10-22 21:51:27 +0800

    1022

---
+++ src/page/content.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>content page</div>
+</template>
+
+<script>
+	// 项目的内容页面
+</script>
+
+<style>
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>index page list</div>
+</template>
+
+<script>
+	// 项目的列表页面
+</script>
+
+<style>
+</style>

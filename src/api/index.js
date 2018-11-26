5c9d2f6f4841bcd340473d9a836fae73f996b961
github1293715794 <1293715794@qq.com>
2018-11-26 13:27:32 +0800

    开始登陆功能开发

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 // 接口调用文件  全局使用
 // 20181124，确定使用node、express框架、连接数据库做接口开发。不使用java
 // 开始进行数据库数据准备工作
+// 20181126 先做一个登陆功能
 let _host = "https://api.github.com";
 let axiosUrl = {
   'users': _host + '/users'

8513cd7e24b2dd4996ab6fa9bcac3db78fc93268
github1293715794 <1293715794@qq.com>
2018-11-29 22:06:51 +0800

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 // 接口调用文件  全局使用
-// 使用node、express框架、连接数据库做接口开发：连接数据库实现了增删改查功能，细节需要优化
+// 使用node、express框架、连接数据库做接口开发：连接数据库实现了增删改查功能，需要优化
 let _host = "https://api.github.com";
 let axiosUrl = {
   'users': _host + '/users'
